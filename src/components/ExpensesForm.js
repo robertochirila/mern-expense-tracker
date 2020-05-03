1c1bd2432f4a581dc92086a0378620f2bd3e4d7c
Roberto Chirila <robertochirila@yahoo.com>
2020-05-03 15:16:51 +0100

    added frontend logic

--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { ExpensesList } from './ExpensesList'
+
 
 export class ExpensesForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            expenseName: "",
+            expenseCategory: "",
+            showList: false
+        }
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        const { expenseName, expenseCategory } = this.state
+        this.setState({ expenseName: "", expenseCategory: "" })
+    }
+
+    handleChange = input => e => {
+
+        e.preventDefault()
+        this.setState({ [input]: e.target.value })
+
+    }
+
+    showList = () => {
+        this.setState({ showList: true })
+    }
+    hideList = () => {
+        this.setState({ showList: false })
+    }
     render() {
+        const { expenseName, expenseCategory, showList } = this.state
         return (
             <div>
+                <Form onSubmit={this.onSubmit}>
+                    <FormGroup>
+                        <Label for="expenseName">Expense Name :</Label>
+                        <Input type="text" name="expense_name" id="expenseName" placeholder="Expense Name" onChange={this.handleChange('expenseName')} value={expenseName} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="expenseCategory">Select Category :</Label>
+                        <Input type="select" name="select" id="expenseCategory" onChange={this.handleChange('expenseCategory')} value={expenseCategory}>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                    </FormGroup>
+                    <Button>Submit</Button>
+                </Form>
+                {showList === false ?
+                    <Button onClick={this.showList}>Show List</Button>
+                    :
+                    <React.Fragment>
+                        <Button onClick={this.hideList}>Hide List</Button>
+                        <ExpensesList />
+                    </React.Fragment>
 
+                }
             </div>
         )
     }
